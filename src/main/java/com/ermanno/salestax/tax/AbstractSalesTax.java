a9ff8c89e556cace5e8a5855edae44c6ff904351
ermanno <moser.ermanno@gmail.com>
2017-01-29 21:14:17 +0100

    fixed rounding problem

--- src/main/java/com/ermanno/salestax/tax/AbstractSalesTax.java
+++ src/main/java/com/ermanno/salestax/tax/AbstractSalesTax.java
@@ -7,7 +7,7 @@ public abstract class AbstractSalesTax implements Tax {
     protected double round(final double number) {
         // See http://stackoverflow.com/questions/11815135/best-method-to-round-up-to-the-nearest-0-05-in-java
         // to understand why this is better than BigDecimal or long.
-        return Math.round(number * 20) / 20.0; // round up to multiple of 0.05
+        return Math.ceil(number * 20) / 20.0; // round up to multiple of 0.05
     }
     
     protected abstract double calculateSalesTax(Item item);
